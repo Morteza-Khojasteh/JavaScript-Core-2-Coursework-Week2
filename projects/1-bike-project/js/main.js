ad67f0f4a5fad80f5a33e704b01c26f7ce0ff059
Mor Khojasteh <mori.kh1987@gmail.com>
2021-08-30 22:13:43 +0100

    Projects

---
+++ projects/1-bike-project/js/main.js
@@ -0,0 +1,61 @@
+const blueBtn = document.querySelector('#blueBtn');
+const jumbotron = document.querySelector('.jumbotron');
+const donateButton =document.querySelector('.buttons .btn-primary');
+const volunteerBtn = document.querySelector('.buttons .btn-secondary');
+blueBtn.addEventListener('click', ()=>{
+  jumbotron.style.backgroundColor = '#3498db';
+  donateButton.style.backgroundColor ='#e67e22'
+  volunteerBtn.style.backgroundColor = '#000';
+  volunteerBtn.style.color = '#fff';
+});
+const orangeBtn = document.querySelector('#orangeBtn');
+orangeBtn.addEventListener('click',()=>{
+  jumbotron.style.backgroundColor = '#f0ad4e';
+  donateButton.style.backgroundColor ='#5751fd'
+  volunteerBtn.style.backgroundColor = '#31b0d5';
+  volunteerBtn.style.color = '#fff';
+
+});
+const greenBtn = document.querySelector('#greenBtn');
+greenBtn.addEventListener('click',()=>{
+  jumbotron.style.backgroundColor = '#2ecc71';
+  donateButton.style.backgroundColor ='#000'
+  volunteerBtn.style.backgroundColor = '#8c9c08';
+  volunteerBtn.style.color = '#fff';
+
+});
+const submitBtn = document.querySelector('form button');
+const email = document.querySelector('#exampleInputEmail1');
+const names = document.querySelector('#example-text-input')
+const describe = document.querySelector('#exampleTextarea');
+submitBtn.addEventListener('click',(e)=>{
+  let allValid = [false,false,false];
+  e.preventDefault();
+  if(email.value.includes('@') && email.value.length >0){
+    email.style.backgroundColor = 'white'
+    allValid[0]=true;
+  } else { 
+    email.style.backgroundColor ='#EC5858';
+    allValid[0]=false;
+   }
+  if(names.value.length ===0){
+    names.style.backgroundColor = '#EC5858';
+    allValid[1]=false;
+  } else {
+  names.style.backgroundColor = 'initial';
+  allValid[1]=true;
+  }
+  if(describe.value.length ===0){
+    describe.style.backgroundColor = '#EC5858';
+    allValid[2]=false;
+  } else {
+  describe.style.backgroundColor = 'initial';
+  allValid[2]=true;
+  }
+  if(!allValid.includes(false)){
+    email.value ='';
+    names.value = '';
+    describe.value ='';
+    alert('thank you!');
+  }
+}) 
--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,12 +1,38 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  list.innerHTML='';
+  todos.forEach((element,index) => {
+    const liEls = document.createElement('li');
+    const i1  = document.createElement('i');
+    const i2 = document.createElement('i');
+    const span = document.createElement('span');
+   liEls.className =('list-group-item d-flex justify-content-between align-items-center');
+   liEls.innerText=element.task;
+   if(element.completed){
+     liEls.style.textDecoration='line-through';
+   }
+   span.classList.add('badge', 'bg-primary', 'rounded-pill');
+   i1.classList.add('fa', 'fa-check');
+   i2.classList.add('fa', 'fa-trash');
+   span.appendChild(i1);
+   span.appendChild(i2);
+   liEls.appendChild(span);
+   list.appendChild(liEls);
+   i1.addEventListener('click',()=>{
+     element.completed =(!element.completed);
+     populateTodoList(todos);
+   })
+   i2.addEventListener('click',()=>{
+     todos.splice(index,1);
+     populateTodoList(todos)
+   })
+ });
 }
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
-  { task: "Wash the dishes", completed: false },
+  { task: "Wash the dishes", completed: true },
   { task: "Do the shopping", completed: false },
 ];
 
@@ -16,7 +42,9 @@ populateTodoList(todos);
 function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  const input = document.querySelector('#todoInput')
+  todos.push({task:[input.value],completed: false})
+  populateTodoList(todos);
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
